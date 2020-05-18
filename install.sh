e4d6a121470315da2678a4d7dd42388b59c1d908
cgkings <58586625+cgkings@users.noreply.github.com>
2020-05-18 23:08:41 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm -rf gd.sh
+wget https://raw.githubusercontent.com/cgkings/gd/master/gd.sh
+echo "输入配置gclone的名称"
+read -p "gclone config Name:" gclone
+sed -i "s/goog/$gclone/g" gd.sh
+chmod +x gd.sh
+echo "请输入 ./gd.sh 使用脚本"
