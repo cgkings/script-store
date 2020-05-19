67622c9eee4e9faf24369c82e2ce95407e3be514
cgking <58586625+cgkings@users.noreply.github.com>
2020-05-19 18:15:25 +0800

    Update gd.sh

--- gd.sh
+++ gd.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+:step1
 echo "输入分享链接"
 read -p "请输入:" link
 link=${link#*id=};
@@ -6,8 +7,18 @@ link=${link#*folders/}
 echo $link
 link=${link#*d/}
 link=${link%?usp*}
+shareid=$link
+j=$(gclone lsd goog:{$shareid} --dump bodies -vv 2>&1 |  grep '^{"shareid"' | grep $shareid)
+rootName=$(echo $j | grep -Po '(?<="name":")[^"]*')
+#copylink=`gclone mkdir goog:{myid}/$link`
+gclone mkdir goog:{myid}/$rootName
+:step2
 #copylink=`gclone copy goog:{$link} goog:{myid} --drive-server-side-across-configs -vvP --min-size 10M --transfers=10`
-gclone mkdir goog:{myid}/$link
 gclone copy goog:{$link} goog:{myid}/$link --drive-server-side-across-configs -vvP --min-size 10M --transfers=10
-gclone copy goog:{$link} goog:{myid}/$link --drive-server-side-across-configs -vvP --min-size 10M --transfers=10
-gclone dedupe newest goog:{myid}/$link
+set /a n+=1
+if %n%==2 goto:step3
+goto:step2
+:step3
+#copylink=`gclone copy goog:{$link} goog:{myid} --drive-server-side-across-configs -vvP --min-size 10M --transfers=10`
+gclone dedupe newest goog:{myid}/$link --drive-server-side-across-configs -v
+goto:step1
