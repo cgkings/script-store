8641e6cefe6fcb5e51c7360d8ffc3cbadeb94ae9
vitaminx <65588810+vitaminx@users.noreply.github.com>
2020-05-20 00:21:58 +0800

    Update gd.sh

--- gd.sh
+++ gd.sh
@@ -9,13 +9,13 @@ link=${link%?usp*}
 id=$link
 j=$(gclone lsd goog:{$id} --dump bodies -vv 2>&1 | grep '^{"id"' | grep $id) rootName=$(echo $j | grep -Po '(?<="name":")[^"]*')
 echo $rootName
-echo 【开始拷贝】
+echo 【开始拷贝】......
 #echo gclone copy goog:{$link} goog:{myid}/$rootName --drive-server-side-across-configs -vvP --transfers=20 --log-file=/root/AutoRclone/LOG/$rootName.txt
 gclone copy goog:{$link} goog:{myid}/$rootName --drive-server-side-across-configs -vvP --transfers=20 --log-file=/root/AutoRclone/LOG/$rootName.txt
-echo 【查缺补漏】
+echo 【查缺补漏】......
 #echo gclone copy goog:{$link} goog:{myid}/$rootName --drive-server-side-across-configs -vvP --transfers=20 --log-file=/root/AutoRclone/LOG/$rootName.txt
 gclone copy goog:{$link} goog:{myid}/$rootName --drive-server-side-across-configs -vvP --transfers=20 --log-file=/root/AutoRclone/LOG/$rootName.txt
-echo 【去重检查】
+echo 【去重检查】......
 #echo gclone dedupe newest goog:{myid}/$rootName --drive-server-side-across-configs -vvP --log-file=/root/AutoRclone/LOG/$rootName.txt
 gclone dedupe newest goog:{myid}/$rootName --drive-server-side-across-configs -vvP --log-file=/root/AutoRclone/LOG/$rootName.txt
-./gd.sh
+#./gd.sh
