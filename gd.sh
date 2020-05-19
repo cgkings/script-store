cfdb659d0373811d3e37e50fbcac7a2006cbf9e2
vitaminx <65588810+vitaminx@users.noreply.github.com>
2020-05-19 19:34:58 +0800

    Update gd.sh

--- gd.sh
+++ gd.sh
@@ -9,9 +9,9 @@ link=${link%?usp*}
 id=$link
 j=$(gclone lsd goog:{$id} --dump bodies -vv 2>&1 | grep '^{"id"' | grep $id) rootName=$(echo $j | grep -Po '(?<="name":")[^"]*')
 echo $rootName
-echo gclone copy goog:{$link} goog:{myid}/$rootName --drive-server-side-across-configs -vvP --min-size 10M --transfers=10
-gclone copy goog:{$link} goog:{myid}/$rootName --drive-server-side-across-configs -vvP --min-size 10M --transfers=10
-echo gclone copy goog:{$link} goog:{myid}/$rootName --drive-server-side-across-configs -vvP --min-size 10M --transfers=10
-gclone copy goog:{$link} goog:{myid}/$rootName --drive-server-side-across-configs -vvP --min-size 10M --transfers=10
-echo gclone dedupe newest goog:{myid}/$rootName --drive-server-side-across-configs -v
-gclone dedupe newest goog:{myid}/$rootName --drive-server-side-across-configs -v
+echo gclone copy goog:{$link} goog:{myid}/$rootName --drive-server-side-across-configs -vvP --transfers=20 --log-file=/root/AutoRclone/LOG/$rootName.txt
+gclone copy goog:{$link} goog:{myid}/$rootName --drive-server-side-across-configs -vvP --transfers=20 --log-file=/root/AutoRclone/LOG/$rootName.txt
+echo gclone copy goog:{$link} goog:{myid}/$rootName --drive-server-side-across-configs -vvP --transfers=20 --log-file=/root/AutoRclone/LOG/$rootName.txt
+gclone copy goog:{$link} goog:{myid}/$rootName --drive-server-side-across-configs -vvP --transfers=20 --log-file=/root/AutoRclone/LOG/$rootName.txt
+echo gclone dedupe newest goog:{myid}/$rootName --drive-server-side-across-configs -vvP --log-file=/root/AutoRclone/LOG/$rootName.txt
+gclone dedupe newest goog:{myid}/$rootName --drive-server-side-across-configs -vvP --log-file=/root/AutoRclone/LOG/$rootName.txt
