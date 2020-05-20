a89e3e0252847b7911a23ee6d1847e6d0d76e613
cgking <58586625+cgkings@users.noreply.github.com>
2020-05-20 15:43:36 +0800

    Update custom_install.sh

--- custom_install.sh
+++ custom_install.sh
@@ -4,5 +4,5 @@ wget https://raw.githubusercontent.com/cgkings/gclone-assistant/master/custom_gd
 echo "输入配置gclone的名称"
 read -p "gclone config Name:" gclone
 sed -i "s/goog/$gclone/g" custom_gd.sh
-chmod +x gd.sh
+chmod +x custom_gd.sh
 echo "请输入 ./custom_gd.sh 使用脚本"
