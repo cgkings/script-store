9bf9d7e9ed17e7b3525b9019961f559f79ed5efd
vitaminx <65588810+vitaminx@users.noreply.github.com>
2020-05-22 00:38:16 +0800

    Update gd.sh

--- gd.sh
+++ gd.sh
@@ -11,9 +11,8 @@ j=$(gclone lsd goog:{$id} --dump bodies -vv 2>&1 | grep '^{"id"' | grep $id) roo
 echo "将文件存入配置目录下文件夹："$rootName
 echo "将日志文件保存在目录：\root\AutoRclone\LOG\下,文件名为"$rootName"的txt文件"
 echo 【开始拷贝】......
-gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k "--log-file=/root/AutoRclone/LOG/$rootName.txt"
+gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k
 echo 【查缺补漏】......
-gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k "--log-file=/root/AutoRclone/LOG/$rootName.txt"
-echo 【去重检查】......
-gclone dedupe newest "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP "--log-file=/root/AutoRclone/LOG/$rootName.txt"
+gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k
+gclone dedupe newest "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP
 #./gd.sh
