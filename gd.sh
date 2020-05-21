031f1d7d9b5128130fdaef5682384246771d2545
vitaminx <65588810+vitaminx@users.noreply.github.com>
2020-05-22 00:31:02 +0800

    Update gd.sh

--- gd.sh
+++ gd.sh
@@ -9,14 +9,11 @@ link=${link%?usp*}
 id=$link
 j=$(gclone lsd goog:{$id} --dump bodies -vv 2>&1 | grep '^{"id"' | grep $id) rootName=$(echo $j | grep -Po '(?<="name":")[^"]*')
 echo "将文件存入配置目录下文件夹："$rootName
-echo "将日志文件保存在目录：\root\AutoRclone\LOG\下,文件名为"$rootName"的txt文件"
+echo "将日志文件保存在目录：\root\AutoRclone\LOG\"$rootName".txt"
 echo 【开始拷贝】......
-#echo gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file="/root/AutoRclone/LOG/$rootName.txt"
-gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file="/root/AutoRclone/LOG/$rootName.txt"
+gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k "--log-file=/root/AutoRclone/LOG/$rootName.txt"
 echo 【查缺补漏】......
-#echo gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file="/root/AutoRclone/LOG/$rootName.txt"
-gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file="/root/AutoRclone/LOG/$rootName.txt"
+gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k "--log-file=/root/AutoRclone/LOG/$rootName.txt"
 echo 【去重检查】......
-#echo gclone dedupe newest "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --log-file="/root/AutoRclone/LOG/$rootName.txt"
-gclone dedupe newest "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --log-file="/root/AutoRclone/LOG/$rootName.txt"
+gclone dedupe newest "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP "--log-file=/root/AutoRclone/LOG/$rootName.txt"
 #./gd.sh
