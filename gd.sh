6d87e24f4bbcf473d6f32173abaa76ae9b48efec
vitaminx <65588810+vitaminx@users.noreply.github.com>
2020-05-22 00:55:01 +0800

    Update gd.sh

--- gd.sh
+++ gd.sh
@@ -9,7 +9,7 @@ link=${link%?usp*}
 id=$link
 j=$(gclone lsd goog:{$id} --dump bodies -vv 2>&1 | grep '^{"id"' | grep $id) rootName=$(echo $j | grep -Po '(?<="name":")[^"]*')
 echo "将文件存入配置目录下文件夹："$rootName
-echo "将日志文件保存在目录：\root\AutoRclone\LOG\"'$rootName.txt'
+echo 将日志文件保存在目录：\root\AutoRclone\LOG\"$rootName".txt
 echo 【开始拷贝】......
 gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k
 echo 【查缺补漏】......
