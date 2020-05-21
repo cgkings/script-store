eb79a172a9862b2020e63101ce0ffd08b85f5047
vitaminx <65588810+vitaminx@users.noreply.github.com>
2020-05-21 21:54:53 +0800

    Update gda.sh

--- gda.sh
+++ gda.sh
@@ -8,16 +8,18 @@ link=${link#*d/}
 link=${link%?usp*}
 id=$link
 j=$(gclone lsd goog:{$id} --dump bodies -vv 2>&1 | grep '^{"id"' | grep $id) rootName=$(echo $j | grep -Po '(?<="name":")[^"]*')
-echo "请输入需要转存到的文件夹ID"
-read -p "文件夹ID:" myid
-echo "文件将转存入："$rootName
+echo "请输入分类文件夹ID"
+read -p "分类文件夹ID:" fid
+aid=$fid
+j=$(gclone lsd goog:{$aid} --dump bodies -vv 2>&1 | grep '^{"aid"' | grep $aid) FName=$(echo $j | grep -Po '(?<="name":")[^"]*')
+echo "文件将转存入："$FName/$rootName
 echo 【开始拷贝】......
-#echo gclone copy goog:{$link} goog:"{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file="/root/AutoRclone/LOG/$rootName".txt
-gclone copy goog:{$link} goog:"{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file="/root/AutoRclone/LOG/$rootName".txt
+#echo gclone copy goog:{$link} goog:"{$fid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file="/root/AutoRclone/LOG/$rootName".txt
+gclone copy goog:{$link} goog:"{$fid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file="/root/AutoRclone/LOG/$rootName".txt
 echo 【查缺补漏】......
-#echo gclone copy goog:{$link} goog:"{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file="/root/AutoRclone/LOG/$rootName".txt
-gclone copy goog:{$link} goog:"{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file="/root/AutoRclone/LOG/$rootName".txt
+#echo gclone copy goog:{$link} goog:"{$fid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file="/root/AutoRclone/LOG/$rootName".txt
+gclone copy goog:{$link} goog:"{$fid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file="/root/AutoRclone/LOG/$rootName".txt
 echo 【去重检查】......
-#echo gclone dedupe newest goog:"{myid}/$rootName" --drive-server-side-across-configs -vvP --log-file="/root/AutoRclone/LOG/$rootName".txt
-gclone dedupe newest goog:"{myid}/$rootName" --drive-server-side-across-configs -vvP --log-file="/root/AutoRclone/LOG/$rootName".txt
-#./gda.sh
+#echo gclone dedupe newest goog:"{$fid}/$rootName" --drive-server-side-across-configs -vvP --log-file="/root/AutoRclone/LOG/$rootName".txt
+gclone dedupe newest goog:"{$fid}/$rootName" --drive-server-side-across-configs -vvP --log-file="/root/AutoRclone/LOG/$rootName".txt
+#./gd.sh
