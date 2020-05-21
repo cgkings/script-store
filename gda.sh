e0d2f64e7bde5768fce30d148b50607f649632a9
vitaminx <65588810+vitaminx@users.noreply.github.com>
2020-05-22 01:26:56 +0800

    Update gda.sh

--- gda.sh
+++ gda.sh
@@ -12,7 +12,7 @@ echo "请输入分类文件夹ID"
 read -p "分类文件夹ID:" folderid
 id=$folderid
 j=$(gclone lsd goog:{$id} --dump bodies -vv 2>&1 | grep '^{"id"' | grep $id) folderName=$(echo $j | grep -Po '(?<="name":")[^"]*')
-echo "文件转存入分类目录："$folderName\$rootName
+echo "文件转存入分类目录："$folderName/$rootName
 echo '日志文件保存在：/root/AutoRclone/LOG/'"$rootName"'.txt'
 echo 【开始拷贝】......
 #echo gclone copy goog:{$link} "goog:{$folderid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file=/root/AutoRclone/LOG/"$rootName"'.txt'
