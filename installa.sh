17d36aed08854dac9634af3874b061cdcbf4902b
vitaminx <65588810+vitaminx@users.noreply.github.com>
2020-05-22 12:01:02 +0800

    Update installa.sh

--- installa.sh
+++ installa.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 rm -rf gda.sh
 wget https://raw.githubusercontent.com/vitaminx/gclone-assistant/master/gda.sh
-echo "输入配置gclone的名称"
-read -p "gclone config Name:" gclone
+#echo "输入配置gclone的名称"
+read -p "请输入配置gclone的名称后按回车键:" gclone
 sed -i "s/goog/$gclone/g" gda.sh
 chmod +x gda.sh
 echo "请输入 ./gda.sh 开始转存"
