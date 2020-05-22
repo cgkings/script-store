ef2579492297910a95d25de716cdddad71a8100c
vitaminx <65588810+vitaminx@users.noreply.github.com>
2020-05-22 19:04:50 +0800

    Update installc.sh

--- installc.sh
+++ installc.sh
@@ -8,9 +8,9 @@ read -p "请输入备份路径1的地址ID后按回车键:" id1
 sed -i "s/Backupid1/$id1/g" gdb.sh
 read -p "请输入备份路径2的地址ID后按回车键:" id2
 sed -i "s/Backupid2/$id2/g" gdb.sh
-read -p "请输入备份路径2的地址ID后按回车键:" id3
+read -p "请输入备份路径3的地址ID后按回车键:" id3
 sed -i "s/Backupid3/$id3/g" gdb.sh
-read -p "请输入备份路径2的地址ID后按回车键:" id4
+read -p "请输入备份路径4的地址ID后按回车键:" id4
 sed -i "s/Backupid4/$id4/g" gdb.sh
 chmod +x gdb.sh
 mkdir -p ~/AutoRclone/LOG/
