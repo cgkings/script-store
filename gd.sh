a452197a30f2ed85bc371b3b3cf0a700d8da8c35
cgking <58586625+cgkings@users.noreply.github.com>
2020-05-22 10:02:04 +0800

    Update gd.sh

--- gd.sh
+++ gd.sh
@@ -9,17 +9,16 @@ link=${link%?usp*}
 id=$link
 j=$(gclone lsd goog:{$id} --dump bodies -vv 2>&1 | grep '^{"id"' | grep $id) rootName=$(echo $j | grep -Po '(?<="name":")[^"]*')
 echo "转存入该文件夹："$rootName
-echo '日志文件保存在：/root/gclone_log/'"$rootName"'.log'
 echo 【开始拷贝】......
-#echo gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 10M --log-file=/root/gclone_log/'"$rootName"'.log'
-gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 10M --log-file=/root/gclone_log/'"$rootName"'.log'
+#echo gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 10M
+gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 10M
 echo 【查缺补漏】......
-#echo gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 10M --log-file=/root/gclone_log/'"$rootName"'.log'
-gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 10M --log-file=/root/gclone_log/'"$rootName"'.log'
+#echo gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 10M
+gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 10M
 echo 【去重检查】......
-#echo gclone dedupe newest "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --log-file=/root/gclone_log/'"$rootName"'.log'
-gclone dedupe newest "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --log-file=/root/gclone_log/'"$rootName"'.log'
+#echo gclone dedupe newest "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP
+gclone dedupe newest "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP
 echo 【比对检查】......
-#echo gclone check goog:{$link} "goog:{myid}/$rootName" --size-only --one-way --no-traverse --log-file=/root/gclone_log/'"$rootName"'.log'
-gclone check goog:{$link} "goog:{myid}/$rootName" --size-only --one-way --no-traverse --log-file=/root/gclone_log/'"$rootName"'.log'
+#echo gclone check goog:{$link} "goog:{myid}/$rootName" --size-only --one-way --no-traverse
+gclone check goog:{$link} "goog:{myid}/$rootName" --size-only --one-way --no-traverse
 #./gd.sh
