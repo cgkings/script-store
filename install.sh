05149fd773a54633b78e3d3d350888d6b0a2d456
vitaminx <65588810+vitaminx@users.noreply.github.com>
2020-05-22 12:00:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 rm -rf gd.sh
 wget https://raw.githubusercontent.com/vitaminx/gclone-assistant/master/gd.sh
-echo "输入配置gclone的名称"
-read -p "gclone config Name:" gclone
+#echo "输入配置gclone的名称"
+read -p "请输入配置gclone的名称后按回车键:" gclone
 sed -i "s/goog/$gclone/g" gd.sh
-echo "请输入需要转存到的固定地址"
-read -p "固定地址ID:" id
+#echo "请输入需要转存到的固定地址"
+read -p "请输入需要转存到的固定地址ID:" id
 sed -i "s/myid/$id/g" gd.sh
 chmod +x gd.sh
 echo "请输入 ./gd.sh 使用脚本"
