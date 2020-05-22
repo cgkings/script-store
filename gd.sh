b9a1a87be1b2198e1cd2e9aead6fe52a227b2f5b
cgking <58586625+cgkings@users.noreply.github.com>
2020-05-22 09:03:27 +0800

    Update gd.sh

--- gd.sh
+++ gd.sh
@@ -3,15 +3,21 @@ echo "输入分享链接"
 read -p "请输入:" link
 link=${link#*id=};
 link=${link#*folders/}
-echo $link
+#echo $link
 link=${link#*d/}
 link=${link%?usp*}
 id=$link
 j=$(gclone lsd goog:{$id} --dump bodies -vv 2>&1 | grep '^{"id"' | grep $id) rootName=$(echo $j | grep -Po '(?<="name":")[^"]*')
-echo $rootName
-echo gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --min-size 10M --transfers=10
-gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --min-size 10M --transfers=10
-echo gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --min-size 10M --transfers=10
-gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --min-size 10M --transfers=10
-echo gclone dedupe newest "goog:{myid}/$rootName" --drive-server-side-across-configs -v
-gclone dedupe newest "goog:{myid}/$rootName" --drive-server-side-across-configs -v
+echo "转存入该文件夹："$rootName
+echo '日志文件保存在：/root/gclone_log/'"$rootName"'.log'
+echo 【开始拷贝】......
+#echo gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 10M --log-file=/root/gclone_log/'"$rootName"'.log'
+gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 10M --log-file=/root/gclone_log/'"$rootName"'.log'
+echo 【查缺补漏】......
+#echo gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 10M --log-file=/root/gclone_log/'"$rootName"'.log'
+gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 10M --log-file=/root/gclone_log/'"$rootName"'.log'
+echo 【去重检查】......
+#echo gclone dedupe newest "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --log-file=/root/gclone_log/'"$rootName"'.log'
+gclone dedupe newest "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --log-file=/root/gclone_log/'"$rootName"'.log'
+echo 【比对检查】......
+#./gd.sh
