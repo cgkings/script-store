8760c8a8897e16694774c14c39962d85d348421c
vitaminx <65588810+vitaminx@users.noreply.github.com>
2020-05-22 14:33:49 +0800

    Update gdb.sh

--- gdb.sh
+++ gdb.sh
@@ -26,8 +26,8 @@ echo 【去重检查】......
 #echo gclone dedupe newest "goog:{$folderid}/$rootName" --drive-server-side-across-configs -vvP --log-file=/root/AutoRclone/LOG/"$rootName"'_dedupe.txt'
 gclone dedupe newest "goog:{$folderid}/$rootName" --drive-server-side-across-configs -vvP --log-file=/root/AutoRclone/LOG/"$rootName"'_dedupe.txt'
 
-if [[ ! -d "gclone lsd goog:{$Backupid1}" ]]; then
-  gclone mkdir "goog:{#Backupid1}/$folderName"
+if [[ ! -d "gclone lsd goog:{Backupid1}" ]]; then
+  gclone mkdir "goog:{Backupid1}/$folderName"
 else
   echo "$folderName"
 fi
@@ -38,17 +38,17 @@ echo '备份1拷贝日志文件将保存在：/root/AutoRclone/LOG/'"$rootName"'
 echo '备份1查漏日志文件将保存在：/root/AutoRclone/LOG/'"$rootName"'_check_backup1.txt'
 echo '备份1去重日志文件将保存在：/root/AutoRclone/LOG/'"$rootName"'_dedupe_backup1.txt'
 echo 【开始拷贝】......
-#echo gclone copy goog:{$link} "goog:{#Backupid1}/$folderName/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file=/root/AutoRclone/LOG/"$rootName"'_backup1.txt'
-gclone copy goog:{$link} "goog:{#Backupid1}/$folderName/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file=/root/AutoRclone/LOG/"$rootName"'_backup1.txt'
+#echo gclone copy goog:{$link} "goog:{Backupid1}/$folderName/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file=/root/AutoRclone/LOG/"$rootName"'_backup1.txt'
+gclone copy goog:{$link} "goog:{Backupid1}/$folderName/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file=/root/AutoRclone/LOG/"$rootName"'_backup1.txt'
 echo 【查缺补漏】......
-#echo gclone copy goog:{$link} "goog:{#Backupid1}/$folderName/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file=/root/AutoRclone/LOG/"$rootName"'_check_backup1.txt'
-gclone copy goog:{$link} "goog:{#Backupid1}/$folderName/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file=/root/AutoRclone/LOG/"$rootName"'_check_backup1.txt'
+#echo gclone copy goog:{$link} "goog:{Backupid1}/$folderName/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file=/root/AutoRclone/LOG/"$rootName"'_check_backup1.txt'
+gclone copy goog:{$link} "goog:{Backupid1}/$folderName/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file=/root/AutoRclone/LOG/"$rootName"'_check_backup1.txt'
 echo 【去重检查】......
-#echo gclone dedupe newest "goog:{#Backupid1}/$folderName/$rootName" --drive-server-side-across-configs -vvP --log-file=/root/AutoRclone/LOG/"$rootName"'_dedupe_backup1.txt'
-gclone dedupe newest "goog:{#Backupid1}/$folderName/$rootName" --drive-server-side-across-configs -vvP --log-file=/root/AutoRclone/LOG/"$rootName"'_dedupe_backup1.txt'
+#echo gclone dedupe newest "goog:{Backupid1}/$folderName/$rootName" --drive-server-side-across-configs -vvP --log-file=/root/AutoRclone/LOG/"$rootName"'_dedupe_backup1.txt'
+gclone dedupe newest "goog:{Backupid1}/$folderName/$rootName" --drive-server-side-across-configs -vvP --log-file=/root/AutoRclone/LOG/"$rootName"'_dedupe_backup1.txt'
 
-if [[ ! -d "gclone lsd goog:{$Backupid2}" ]]; then
-  gclone mkdir "goog:{#Backupid2}/$folderName"
+if [[ ! -d "gclone lsd goog:{Backupid2}" ]]; then
+  gclone mkdir "goog:{Backupid2}/$folderName"
 else
   echo "$folderName"
 fi
@@ -59,13 +59,13 @@ echo '备份2拷贝日志文件将保存在：/root/AutoRclone/LOG/'"$rootName"'
 echo '备份2查漏日志文件将保存在：/root/AutoRclone/LOG/'"$rootName"'_check_backup2.txt'
 echo '备份2去重日志文件将保存在：/root/AutoRclone/LOG/'"$rootName"'_dedupe_backup2.txt'
 echo 【开始拷贝】......
-#echo gclone copy goog:{$link} "goog:{#Backupid2}/$folderName/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file=/root/AutoRclone/LOG/"$rootName"'_backup2.txt'
-gclone copy goog:{$link} "goog:{#Backupid2}/$folderName/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file=/root/AutoRclone/LOG/"$rootName"'_backup2.txt'
+#echo gclone copy goog:{$link} "goog:{Backupid2}/$folderName/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file=/root/AutoRclone/LOG/"$rootName"'_backup2.txt'
+gclone copy goog:{$link} "goog:{Backupid2}/$folderName/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file=/root/AutoRclone/LOG/"$rootName"'_backup2.txt'
 echo 【查缺补漏】......
-#echo gclone copy goog:{$link} "goog:{#Backupid2}/$folderName/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file=/root/AutoRclone/LOG/"$rootName"'_check_backup2.txt'
-gclone copy goog:{$link} "goog:{#Backupid2}/$folderName/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file=/root/AutoRclone/LOG/"$rootName"'_check_backup2.txt'
+#echo gclone copy goog:{$link} "goog:{Backupid2}/$folderName/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file=/root/AutoRclone/LOG/"$rootName"'_check_backup2.txt'
+gclone copy goog:{$link} "goog:{Backupid2}/$folderName/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file=/root/AutoRclone/LOG/"$rootName"'_check_backup2.txt'
 echo 【去重检查】......
-#echo gclone dedupe newest "goog:{#Backupid2}/$folderName/$rootName" --drive-server-side-across-configs -vvP --log-file=/root/AutoRclone/LOG/"$rootName"'_dedupe_backup2.txt'
-gclone dedupe newest "goog:{#Backupid2}/$folderName/$rootName" --drive-server-side-across-configs -vvP --log-file=/root/AutoRclone/LOG/"$rootName"'_dedupe_backup2.txt'
+#echo gclone dedupe newest "goog:{Backupid2}/$folderName/$rootName" --drive-server-side-across-configs -vvP --log-file=/root/AutoRclone/LOG/"$rootName"'_dedupe_backup2.txt'
+gclone dedupe newest "goog:{Backupid2}/$folderName/$rootName" --drive-server-side-across-configs -vvP --log-file=/root/AutoRclone/LOG/"$rootName"'_dedupe_backup2.txt'
 
 #./gd.sh
