e1225cb244b0c861f1c48651f2a4075ec95c20cd
vitaminx <65588810+vitaminx@users.noreply.github.com>
2020-05-22 14:35:36 +0800

    Update installb.sh

--- installb.sh
+++ installb.sh
@@ -5,9 +5,6 @@ wget https://raw.githubusercontent.com/vitaminx/gclone-assistant/master/gdb.sh
 echo 本脚本将执行：复制一份，备份两份，共计三份相同档案
 read -p "请输入配置gclone的名称后按回车键:" gclone
 sed -i "s/goog/$gclone/g" gdb.sh
-#echo "请输入需要转存到的固定地址"
-read -p "请输入需要转存到的固定地址ID后按回车键:" id
-sed -i "s/myid/$id/g" gdb.sh
 read -p "请输入备份路径1的地址ID后按回车键:" id1
 sed -i "s/Backupid1/$id1/g" gdb.sh
 read -p "请输入备份路径2的地址ID后按回车键:" id2
