217ed6c140ca578f5827bc31d22d52d77bf21e89
cgking <58586625+cgkings@users.noreply.github.com>
2020-05-23 15:01:47 +0800

    Update 自定义备份installa.sh

--- 自定义备份installa.sh
+++ 自定义备份installa.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-rm -rf gda.sh
-wget https://raw.githubusercontent.com/cgkings/gclone-assistant/master/gda.sh
+rm -rf gd.sh
+wget https://raw.githubusercontent.com/cgkings/gclone-assistant/master/gdt.sh
 echo "输入配置gclone的名称"
 read -p "gclone config Name:" gclone
-sed -i "s/goog/$gclone/g" gda.sh
-chmod +x gda.sh
-echo "请输入 ./gda.sh 开始转存"
+sed -i "s/goog/$gclone/g" gd.sh
+echo "请输入需要转存到的固定地址"
+read -p "固定地址ID:" mid
+sed -i "s/myid/$mid/g" gd.sh
+echo "请输入固定地址所在团队盘地址"
+read -p "固定地址团队盘ID:" tid
+sed -i "s/tdid/$tid/g" gd.sh
+echo "请输入备份盘的全盘地址"
+read -p "全盘地址ID:" bid
+sed -i "s/bakid/$bid/g" gd.sh
+chmod +x gd.sh
+echo "请输入 ./gd.sh 使用脚本"
