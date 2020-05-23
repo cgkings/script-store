d69bdde9d8240a0b0f8efd93ba69af99cda4dcd7
vitaminx <65588810+vitaminx@users.noreply.github.com>
2020-05-23 22:30:57 +0800

    Update gdx.sh

--- gdx.sh
+++ gdx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -e "TD分享链接一键转存脚本 4 in 1版 ${Red_font_prefix}[v1.0 ${Font_color_suffix} by \033[1;35mcgkings&oneking\033[0m]"
+echo -e "TD分享链接一键转存脚本 4 in 1版 ${Red_font_prefix}[ v1.0 ${Font_color_suffix} by \033[1;35mcgkings & oneking\033[0m ]"
 read -p """输入分享链接
      请输入 =>:""" link
 # 检查接受到的分享链接规范性，并读取分享文件夹ID和文件夹名
@@ -14,21 +14,22 @@ link=${link%?usp*}
 id=$link
 j=$(gclone lsd goog:{$id} --dump bodies -vv 2>&1 | grep '^{"id"' | grep $id) rootName=$(echo $j | grep -Po '(?<="name":")[^"]*')
 check_results=`gclone size goog:{"$link"} 2>&1`
-    if [[ $check_results =~ "Error 404" ]]
-    then
+	if [[ $check_results =~ "Error 404" ]]
+	then
     echo "链接无效，检查是否有权限" && exit
     else
     echo "分享链接的基本信息如下："
 	echo "分享目录名："$rootName""
 	echo "分享目录下文件数和总大小："$check_results""
-	echo -e "\n"
-    echo "请输入1~4选择转存模式,直接回车或5秒钟未输入自动选择“急速转存模式”"
+	echo -e
+    echo -n "请输入1~4选择转存模式,直接回车或5秒钟未输入自动选择“急速转存模式”"
     fi
 fi
 # 进行选项操作，默认1急速转存
 
-run_gd_fast() {
-echo -e '==<<极速转存即将开始，可ctrl+c中途中断>>=='
+run_gd_fast(){
+echo -e "\n"
+echo '==<<极速转存即将开始，可ctrl+c中途中断>>=='
 echo -e
 echo "文件将转存到配置中固定目录下文件夹：$rootName"
 echo '转存日志文件将保存在：/root/AutoRclone/LOG/'"$rootName"'.txt'
@@ -76,6 +77,7 @@ echo 【查缺补漏】......
 gclone copy goog:{$link} "goog:{$folderid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file=/root/AutoRclone/LOG/"$rootName"'_check.txt'
 echo 【去重检查】......
 gclone dedupe newest "goog:{$folderid}/$rootName" --drive-server-side-across-configs -vvP --log-file=/root/AutoRclone/LOG/"$rootName"'_dedupe.txt'
+echo -e
 
 if [[ ! -d "gclone lsd goog:{Backupid1}" ]]; then
 	gclone mkdir "goog:{Backupid1}/$folderName"
@@ -95,9 +97,10 @@ gclone copy goog:{$link} "goog:{Backupid1}/$folderName/$rootName" --drive-server
 echo 【备份去重检查】......
 gclone dedupe newest "goog:{Backupid1}/$folderName/$rootName" --drive-server-side-across-configs -vvP --log-file=/root/AutoRclone/LOG/"$rootName"'_dedupe_backup1.txt'
 }
+
 run_gd_bak4() {
 read -p "请输入分类文件夹ID后按回车键:" folderid
-echo "==<<分类转存"&"多备份模式即将开始（转存一份、备份四份、共五份），可ctrl+c中途中断>>=="
+echo "==<<分类转存&多备份模式即将开始（转存一份、备份四份、共五份），可ctrl+c中途中断>>=="
 id=$folderid
 j=$(gclone lsd goog:{$id} --dump bodies -vv 2>&1 | grep '^{"id"' | grep $id) folderName=$(echo $j | grep -Po '(?<="name":")[^"]*')
 echo "文件将转存入分类目录："$folderName/$rootName
@@ -110,6 +113,7 @@ echo 【查缺补漏】......
 gclone copy goog:{$link} "goog:{$folderid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file=/root/AutoRclone/LOG/"$rootName"'_check.txt'
 echo 【去重检查】......
 gclone dedupe newest "goog:{$folderid}/$rootName" --drive-server-side-across-configs -vvP --log-file=/root/AutoRclone/LOG/"$rootName"'_dedupe.txt'
+echo -e
 
 if [[ ! -d "gclone lsd goog:{Backupid1}" ]]; then
   gclone mkdir "goog:{Backupid1}/$folderName"
@@ -128,6 +132,7 @@ echo 【备份一查缺补漏】......
 gclone copy goog:{$link} "goog:{Backupid1}/$folderName/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file=/root/AutoRclone/LOG/"$rootName"'_check_backup1.txt'
 echo 【备份一去重检查】......
 gclone dedupe newest "goog:{Backupid1}/$folderName/$rootName" --drive-server-side-across-configs -vvP --log-file=/root/AutoRclone/LOG/"$rootName"'_dedupe_backup1.txt'
+echo -e
 
 if [[ ! -d "gclone lsd goog:{Backupid2}" ]]; then
   gclone mkdir "goog:{Backupid2}/$folderName"
@@ -146,6 +151,7 @@ echo 【备份二查缺补漏】......
 gclone copy goog:{$link} "goog:{Backupid2}/$folderName/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file=/root/AutoRclone/LOG/"$rootName"'_check_backup2.txt'
 echo 【备份二去重检查】......
 gclone dedupe newest "goog:{Backupid2}/$folderName/$rootName" --drive-server-side-across-configs -vvP --log-file=/root/AutoRclone/LOG/"$rootName"'_dedupe_backup2.txt'
+echo -e
 
 if [[ ! -d "gclone lsd goog:{Backupid3}" ]]; then
   gclone mkdir "goog:{Backupid3}/$folderName"
@@ -164,6 +170,7 @@ echo 【备份三查缺补漏】......
 gclone copy goog:{$link} "goog:{Backupid3}/$folderName/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 100k --log-file=/root/AutoRclone/LOG/"$rootName"'_check_backup3.txt'
 echo 【备份三去重检查】......
 gclone dedupe newest "goog:{Backupid3}/$folderName/$rootName" --drive-server-side-across-configs -vvP --log-file=/root/AutoRclone/LOG/"$rootName"'_dedupe_backup3.txt'
+echo -e
 
 if [[ ! -d "gclone lsd goog:{Backupid4}" ]]; then
   gclone mkdir "goog:{Backupid4}/$folderName"
@@ -191,8 +198,8 @@ echo && echo -e "
  ${Green_font_prefix} 3.${Font_color_suffix} 分类转存&备份：转存入输入的分类文件夹并建立一个备份
  ———————————————————————
  ${Green_font_prefix} 4.${Font_color_suffix} 分类转存&多备份：转存入输入的分类文件夹并建立四个备份
- ———————————————————————"&& echo
-read -t 5 -e -p "请输入数字 [1-4]:" num
+ ———————————————————————"&& echo -n
+read -t 10 -p "请输入数字 [1-4]:" num
 num=${num:-1}
 case "$num" in
 1)
