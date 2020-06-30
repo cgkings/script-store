b44f79117587661befcb34ef3ca46fa9e76e19a0
cgking <58586625+cgkings@users.noreply.github.com>
2020-06-30 22:07:58 +0800

    归类更新

---
+++ script/fmod/fmodinstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+rm -rf fmod.sh
+wget https://raw.githubusercontent.com/cgkings/fmod-assistant/master/gd.sh
+echo "【fmod一键转存脚本自用】系统配置"
+read -p "输入配置fmod的名称:" fmodid
+sed -i "s/gc/$fmodid/g" fmod.sh
+read -p "请输入需要转存到的固定地址ID:" tdid
+sed -i "s/myid/$tdid/g" fmod.sh
+chmod +x fmod.sh
+echo "请输入 ./fmod.sh 使用脚本"
