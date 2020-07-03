94c77e09ac849e46265180fe5ff32f5f02b4f011
cgking <58586625+cgkings@users.noreply.github.com>
2020-07-03 14:52:39 +0800

    Update fmod.sh

--- script/fmod/fmod.sh
+++ script/fmod/fmod.sh
@@ -24,37 +24,38 @@ check_results=`fmod size goog:{"$link"} --checkers=256 --drive-pacer-min-sleep=1
     fi
 fi
 echo -e " fmod自用版 [ v1.0 by \e[1;34m cgkings \e[0m ]
+请输入数字 [0-4]: (10s默认选0)
 [0]. 中转盘ID转存
 [1]. ADV盘ID转存
 [2]. MDV盘ID转存
 [3]. BOOK盘ID转存
 [4]. 自定义ID转存"
-read -t 10 -n1 -p " 请输入数字 [0-4]: (10s默认选0)" num
+read -t 10 -n1 -p -s num
 num=${num:-0}
 case "$num" in
 0)
-    echo "你选择的是：0#中转盘ID，如选错可ctrl+c中断该转存任务"
-    echo "==<<极速转存即将开始>>=="
+    echo -e "你选择的是：0#中转盘ID，如选错可ctrl+c中断该转存任务"
+    echo -e "==<<极速转存即将开始>>=="
     myid=myid1
     ;;
 1)
-    echo "你选择的是：1#ADV盘ID，如选错可ctrl+c中断该转存任务"
-    echo "==<<极速转存即将开始>>=="
+    echo -e "你选择的是：1#ADV盘ID，如选错可ctrl+c中断该转存任务"
+    echo -e "==<<极速转存即将开始>>=="
     myid=myid2
     ;;
 2)
-    echo "你选择的是：2#MDV盘ID，如选错可ctrl+c中断该转存任务"
-    echo "==<<极速转存即将开始>>=="
+    echo -e "你选择的是：2#MDV盘ID，如选错可ctrl+c中断该转存任务"
+    echo -e "==<<极速转存即将开始>>=="
     myid=myid3
     ;;
 3)
-    echo "你选择的是：3#BOOK盘ID，如选错可ctrl+c中断该转存任务"
-    echo "==<<极速转存即将开始>>=="
+    echo -e "你选择的是：3#BOOK盘ID，如选错可ctrl+c中断该转存任务"
+    echo -e "==<<极速转存即将开始>>=="
     myid=myid4
     ;;
 4)
-    read -p "你选择的是：4#自定义ID转存
-            请输入自定义转存ID:" myid5
+    echo -e "你选择的是：4#自定义ID转存，如选错可ctrl+c中断该转存任务"
+    read -p "请输入自定义转存ID:" myid5
     myid=$myid5
     ;;
 *)
@@ -74,8 +75,9 @@ echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  查重完毕"
 echo 【比对检查】......
 fmod check goog:{$link} goog:{$myid}/"$rootName" --fast-list --size-only --one-way --no-traverse --min-size 10M --checkers=320 --drive-pacer-min-sleep=1ms
 echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  检查完毕"
-echo "请注意清空回收站，群组账号必须对团队盘有管理员权限"
-read -t 10 -n1 -p "是否要清空回收站 [Y/N]? 10s不选默认N" answer
+echo -e "请注意清空回收站，群组账号必须对团队盘有管理员权限"
+echo -e "是否要清空回收站 [Y/N]? 10s不选默认N"
+read -t 10 -n1 -p -s answer
 answer=${answer:-N}
 case "$answer" in
 Y | y)
