de69e20fc5ccbe4569c031b82933bb3ad6d4eb39
cgking <58586625+cgkings@users.noreply.github.com>
2020-07-03 18:08:43 +0800

    更新fclone

--- script/fclone/fclone.sh
+++ script/fclone/fclone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Author: cgking
 # Created Time : 2020.7.1
-# File Name: fmod.sh
+# File Name: fclone.sh
 # Description:
 read -p "请输入分享链接==>" link
 # 检查接受到的分享链接规范性，并转化出分享文件ID
@@ -13,8 +13,8 @@ link=${link#*folders/};
 link=${link#*d/};
 link=${link%?usp*}
 id=$link
-rootName=$(fmod lsf goog:{$link} --dump bodies -vv 2>&1 | grep '"$link","name"' | cut -d '"' -f 8)
-echo -e " Fmod自用版 [ v1.0 by \e[1;34m cgkings \e[0m ]
+rootName=$(fclone lsf goog:{$link} --dump bodies -vv 2>&1 | grep '"$link","name"' | cut -d '"' -f 8)
+echo -e " fclone自用版 [ v1.0 by \e[1;34m cgkings \e[0m ]
 请输入数字 [0-4]: (10s默认选0)
 [0]. 中转盘ID转存
 [1]. ADV盘ID转存
@@ -72,16 +72,16 @@ case "$num" in
     esac
 fi
 echo -e "▣▣▣▣▣▣▣正在执行转存▣▣▣▣▣▣▣\n"
-fmod copy goog:{$link} goog:{$myid}/"$rootName" --drive-server-side-across-configs --stats=1s --stats-one-line -vP --checkers=256 --transfers=320 --drive-pacer-min-sleep=1ms --check-first --min-size 10M --log-file=/root/gclone_log/"$rootName"'_copy1.txt'
+fclone copy goog:{$link} goog:{$myid}/"$rootName" --drive-server-side-across-configs --stats=1s --stats-one-line -vP --checkers=256 --transfers=320 --drive-pacer-min-sleep=1ms --check-first --min-size 10M --log-file=/root/gclone_log/"$rootName"'_copy1.txt'
 echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  拷贝完毕"
 echo -e "▣▣▣▣▣▣▣正在执行同步▣▣▣▣▣▣▣\n"
-fmod sync goog:{$link} goog:{$myid}/"$rootName" --drive-server-side-across-configs --stats=1s --stats-one-line -vP --checkers=256 --transfers=320 --drive-pacer-min-sleep=1ms --check-first --min-size 10M --drive-use-trash=false --log-file=/root/gclone_log/"$rootName"'_copy1.txt'
+fclone sync goog:{$link} goog:{$myid}/"$rootName" --drive-server-side-across-configs --stats=1s --stats-one-line -vP --checkers=256 --transfers=320 --drive-pacer-min-sleep=1ms --check-first --min-size 10M --drive-use-trash=false --log-file=/root/gclone_log/"$rootName"'_copy1.txt'
 echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  同步完毕"
 echo -e "▣▣▣▣▣▣▣正在执行查重▣▣▣▣▣▣▣\n"
-fmod dedupe newest goog:{$myid}/"$rootName" --fast-list --drive-use-trash=false --no-traverse --size-only -v --log-file=/root/gclone_log/"$rootName"'_dedupe.txt'
+fclone dedupe newest goog:{$myid}/"$rootName" --fast-list --drive-use-trash=false --no-traverse --size-only -v --log-file=/root/gclone_log/"$rootName"'_dedupe.txt'
 echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  查重完毕"
 echo -e "▣▣▣▣▣▣▣正在执行比对▣▣▣▣▣▣▣\n"
-fmod check goog:{$link} goog:{$myid}/"$rootName" --fast-list --size-only --one-way --no-traverse --min-size 10M --checkers=320 --drive-pacer-min-sleep=1ms
+fclone check goog:{$link} goog:{$myid}/"$rootName" --fast-list --size-only --one-way --no-traverse --min-size 10M --checkers=320 --drive-pacer-min-sleep=1ms
 echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  比对完毕"
 echo
 echo -e "请注意清空回收站，群组账号必须对团队盘有管理员权限,10s不选默认N\n"
@@ -91,8 +91,8 @@ answer=${answer:-N}
 case "$answer" in
 Y | y)
     echo -e "▣▣▣▣▣▣▣▣清空回收站▣▣▣▣▣▣▣▣\n"
-    fmod delete goog:{$myid} --fast-list --drive-trashed-only --drive-use-trash=false --drive-server-side-across-configs --checkers=256 --transfers=128 --drive-pacer-min-sleep=1ms --drive-pacer-burst=5000 --check-first --log-level INFO --log-file=/root/gclone_log/"$rootName"'_trash.txt'
-    fmod rmdirs goog:{$myid} --fast-list --drive-trashed-only --drive-use-trash=false --drive-server-side-across-configs --checkers=256 --transfers=128 --drive-pacer-min-sleep=1ms --drive-pacer-burst=5000 --check-first --log-level INFO --log-file=/root/gclone_log/"$rootName"'_rmdirs.txt'
+    fclone delete goog:{$myid} --fast-list --drive-trashed-only --drive-use-trash=false --drive-server-side-across-configs --checkers=256 --transfers=128 --drive-pacer-min-sleep=1ms --drive-pacer-burst=5000 --check-first --log-level INFO --log-file=/root/gclone_log/"$rootName"'_trash.txt'
+    fclone rmdirs goog:{$myid} --fast-list --drive-trashed-only --drive-use-trash=false --drive-server-side-across-configs --checkers=256 --transfers=128 --drive-pacer-min-sleep=1ms --drive-pacer-burst=5000 --check-first --log-level INFO --log-file=/root/gclone_log/"$rootName"'_rmdirs.txt'
     echo -e "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  回收站清空完毕\n"
     echo -e "日志文件存储路径/root/gclone_log/"$rootName"_(copy1/copy2/dedupe/trash/rmdirs).txt\n"
     ;;
@@ -105,4 +105,4 @@ N | n)
     echo -e " ${Error} 请输入正确的数字"
     ;;
 esac
-./fmod.sh
+./fclone.sh
