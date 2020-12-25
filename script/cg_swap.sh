4f697099d645f28bd9f824f11f736fc7a98c21ac
cgking <58586625+cgkings@users.noreply.github.com>
2020-12-26 01:07:21 +0800

    添加脚本执行参数

--- script/cg_swap.sh
+++ script/cg_swap.sh
@@ -117,14 +117,13 @@ del_swap(){
 
 #帮助
 swap_help(){
-  echo -e "${Red}用法(Usage):
-                     bash <(curl -s -L https://git.io/cg_swap) [flags]${Font}
-                 可用参数(Available flags)：
-                 bash <(curl -s -L https://git.io/cg_swap) a  自动添加swap
-                 bash <(curl -s -L https://git.io/cg_swap) m  手动添加swap
-                 bash <(curl -s -L https://git.io/cg_swap) d  删除现有swap
-                 bash <(curl -s -L https://git.io/cg_swap) h  命令帮助
-                 注：无参数则进入主菜单"                     
+  echo -e "${Red}用法(Usage):bash <(curl -s -L https://git.io/cg_swap) [flags]
+  可用参数(Available flags)：
+  bash <(curl -s -L https://git.io/cg_swap) a  自动添加swap
+  bash <(curl -s -L https://git.io/cg_swap) m  手动添加swap
+  bash <(curl -s -L https://git.io/cg_swap) d  删除现有swap
+  bash <(curl -s -L https://git.io/cg_swap) h  命令帮助
+  注：无参数则进入主菜单${Font}"                     
 }
 
 #开始菜单
