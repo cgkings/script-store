818583189ba648f7adefa903c54f525ff1c13aec
cgking <58586625+cgkings@users.noreply.github.com>
2021-02-23 14:05:15 +0800

    Update fsingle.sh

--- script/fsingle.sh
+++ script/fsingle.sh
@@ -24,11 +24,11 @@ read -r -p "请输入要提取单文件的文件夹id==>>" from_id
 #选择要操作的remote
 remote_choose
 #删除小文件
-fclone delete $my_remote:{"$from_id"} --drive-use-trash=false --ignore-checksum -vvP --checkers=16 --transfers=16 --check-first --max-size 100M --ignore-errors
+fclone delete $my_remote:{"$from_id"} --drive-use-trash=false -vv --checkers=16 --transfers=16 --check-first --max-size 100M --ignore-errors
 #提取单文件到当前目录
 rclone lsf $my_remote: --files-only --format "p" -R --drive-root-folder-id $from_id | xargs -t -n1 -I {} rclone move $my_remote:/{} $my_remote: --drive-server-side-across-configs --check-first --stats=1s --stats-one-line -vP --delete-empty-src-dirs --ignore-errors --drive-root-folder-id $from_id
 #按hash查重
-rclone dedupe $my_remote: --dedupe-mode largest --by-hash -vv --drive-use-trash=false --delete-empty-src-dirs --ignore-errors --drive-root-folder-id $from_id
+rclone dedupe $my_remote: --dedupe-mode largest --by-hash -vv --drive-use-trash=false --ignore-errors --drive-root-folder-id $from_id
 #删除空文件夹
 fclone rmdirs $my_remote:{"$from_id"} --fast-list --drive-use-trash=false -vv --checkers=16 --transfers=8 --drive-pacer-min-sleep=5ms --drive-pacer-burst=1000 --ignore-errors
 exit
