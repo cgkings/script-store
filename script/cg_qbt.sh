f7fc6c2f30d4d49b5a31a908ea7dfa1096e1a545
cgking <cgkings@gmail.com>
2021-06-01 01:31:52 +0800

    Update cg_qbt.sh

--- script/cg_qbt.sh
+++ script/cg_qbt.sh
@@ -147,9 +147,8 @@ qb_del() {
 ################## 主执行模块 ##################
 check_rclone
 check_qbt
-if [ ! -d /home/qbt ]
-then
-	mkdir -p /home/qbt
+if [ ! -d /home/qbt ]; then
+  mkdir -p /home/qbt
 fi
 if [ -z "$content_dir" ]; then
   echo -e "$(date '+%Y-%m-%d %H:%M:%S') [ERROR] 无种子信息，脚本停止运行" >> /home/qbt/qb.log
