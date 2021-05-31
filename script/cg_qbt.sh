6c7518aed60963e7a6bac2753083efb57d7a2747
cgking <cgkings@gmail.com>
2021-05-31 11:50:08 +0800

    update

--- script/cg_qbt.sh
+++ script/cg_qbt.sh
@@ -16,26 +16,19 @@
 # shellcheck source=/dev/null
 source <(curl -sL git.io/cg_script_option)
 setcolor
-#bash /root/qb_auto.sh  "%N" "%F" "%R" "%D" "%C" "%Z" "%I" "%L"
-""
+#/root/cg_qbt.sh "%N" "%F" "%C" "%Z" "%I" "%L"
 torrent_name=$1 # %N：Torrent名称=mide-007-C
 content_dir=$2 # %F：内容路径=/home/btzz/mide-007-C
-root_dir=$3 # %R：根目录=/home/btzz/mide-007-C
-save_dir=$4 # %D：保存路径=/home/btzz/
-files_num=$5 # %C
-torrent_size=$6 #%Z
-file_hash=$7 #%I
-file_category=$8 #%L：分类=btzz
+files_num=$3 # %C
+torrent_size=$4 #%Z
+file_hash=$5 #%I
+file_category=$6 #%L：分类=btzz
 
 qb_version="4.3.5.10"
-qb_username="hostloc"
-qb_password="hostloc.com"
-qb_web_url="http://localhost:8080"
-leeching_mode="true"
-log_dir="/root/qbauto"
-rclone_dest="gdrive"
-rclone_parallel="32"
-auto_del_flag="rclone"
+qb_username="cgking"
+qb_password="340622"
+qb_web_url="$(hostname -I | awk '{print $1}'):8070"
+rclone_remote="upsa"
 
 ################## 检查qbt安装情况 ##################
 check_qbt() {
@@ -74,7 +67,16 @@ RestartSec=3s
 WantedBy=multi-user.target
 EOF
     systemctl daemon-reload && systemctl enable qbt.service && systemctl restart qbt.service
-    clear
+    cat >> /home/qbt/qb.log << EOF
+-----------------------------------------------------------------------------
+$(date '+%Y-%m-%d %H:%M:%S') [INFO] install done！
+-----------------------------------------------------------------------------
+程序名称：qBittorrent-Enhanced-Edition
+版本名称：4.3.5.10
+程序目录：/usr/bin/qbittorrent-nox
+服务地址：/etc/systemd/system/qbt.service
+-----------------------------------------------------------------------------
+EOF
   fi
 }
 
@@ -92,30 +94,61 @@ EOF
 # }
 
 ################## rclone上传模块 ##################
-rclone_move() {
-  if [ ${type} == "file" ]; then
-    rclone_copy_cmd=$(rclone -v copy --transfers ${rclone_parallel} --log-file ${log_dir}/qbauto_copy.log "${content_dir}" ${rclone_dest}:qbauto/)
-  elif [ ${type} == "dir" ]; then
-    rclone_copy_cmd=$(rclone -v copy --transfers ${rclone_parallel} --log-file ${log_dir}/qbauto_copy.log "${content_dir}"/ ${rclone_dest}:qbauto/"${torrent_name}")
+rclone_upload() {
+  if [ -z "${file_category}" ]; then
+    rclone_dest="{0AAa0DHcTPGi9Uk9PVA}"
+  elif [ "${file_category}" == "chs" ]; then
+    rclone_dest="{0AAa0DHcTPGi9Uk9PVA}"
+  elif [ "${file_category}" == "fc2" ]; then
+    rclone_dest="{0AAa0DHcTPGi9Uk9PVA}"
+  elif [ "${file_category}" == "suren" ]; then
+    rclone_dest="{0AAa0DHcTPGi9Uk9PVA}"
+  elif [ "${file_category}" == "pohuaiban" ]; then
+    rclone_dest="{0AAa0DHcTPGi9Uk9PVA}"
+  else
+    rclone_dest="{0AAa0DHcTPGi9Uk9PVA}"
+  fi
+  cat >> /home/qbt/qb.log << EOF
+-----------------------------------------------------------------------------
+种子名称：${torrent_name}
+内容路径：${content_dir}
+文件数量：${files_num}
+文件大小：${torrent_size} Bytes
+文件哈希: ${file_hash}
+分类名称：${file_category}
+-----------------------------------------------------------------------------
+$(date '+%Y-%m-%d %H:%M:%S') [INFO] $torrent_name 分类=$file_category 开始上传
+-----------------------------------------------------------------------------
+EOF
+  fclone move "$content_dir" "$rclone_remote":"$rclone_dest" --use-mmap --stats=10s --stats-one-line -vvP --min-size 100M --log-file=/home/qbt/bt_upload.log
+  RCLONE_EXIT_CODE=$?
+  if [ ${RCLONE_EXIT_CODE} -eq 0 ]; then
+    echo -e "$(date '+%Y-%m-%d %H:%M:%S') [INFO] Upload done: ${torrent_name} -> ${rclone_remote}:${rclone_dest}" >> /home/qbt/qb.log
+  else
+    echo -e "$(date '+%Y-%m-%d %H:%M:%S') [ERROR] Upload failed: ${torrent_name}" >> /home/qbt/qb.log
+    exit 1
+  fi
+}
+
+################## rclone上传模块 ##################
+qb_del() {
+  cookie=$(curl -i --header "Referer: ${qb_web_url}" --data "username=${qb_username}&password=${qb_password}" "${qb_web_url}/api/v2/auth/login" | grep -P -o 'SID=\S{32}')
+  if [ -n "${cookie}" ]; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') [INFO] 登录成功！cookie:${cookie}" >> /home/qbt/qb.log
+    curl "${qb_web_url}/api/v2/torrents/delete?hashes=${file_hash}&deleteFiles=true" --cookie "$cookie"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') [INFO] 删除成功！种子名称:${torrent_name}" >> /home/qbt/qb.log
+  else
+    echo "$(date '+%Y-%m-%d %H:%M:%S') [ERROR] 登录失败！" >> /home/qbt/qb.log
+    exit 1
   fi
 }
 
 ################## 主执行模块 ##################
 check_qbt
 if [ -z "$content_dir" ]; then
-  echo
+  echo -e "$(date '+%Y-%m-%d %H:%M:%S') [ERROR] 无种子信息，脚本停止运行" >> /home/qbt/qb.log
+  exit 1 #异常退出
 else
-  rclone_move
-  qb_login
-  qb_add_auto_del_tags
+  rclone_upload
   qb_del
-  echo "种子名称：${torrent_name}" >> ${log_dir}/qb.log
-  echo "内容路径：${content_dir}" >> ${log_dir}/qb.log
-  echo "根目录：${root_dir}" >> ${log_dir}/qb.log
-  echo "保存路径：${save_dir}" >> ${log_dir}/qb.log
-  echo "文件数：${files_num}" >> ${log_dir}/qb.log
-  echo "文件大小：${torrent_size}Bytes" >> ${log_dir}/qb.log
-  echo "HASH:${file_hash}" >> ${log_dir}/qb.log
-  echo "Cookie:${cookie}" >> ${log_dir}/qb.log
-  echo -e "-------------------------------------------------------------\n" >> ${log_dir}/qb.log
 fi
