a5e07946617e1ee3af6c3b65d7e983b6076cb916
cgking <cgkings@gmail.com>
2021-06-21 01:29:19 +0800

    Update cg_qbt.sh

--- script/cg_qbt.sh
+++ script/cg_qbt.sh
@@ -119,7 +119,7 @@ EOF
 
 ################## rclone上传模块 ##################
 rclone_upload() {
-  fclone move "$content_dir" "$rclone_remote":"$rclone_dest" --use-mmap --stats=10s --stats-one-line -vP --min-size 100M --log-file=/home/qbt/bt_upload.log
+  fclone move "$content_dir" "$rclone_remote":"$rclone_dest" --use-mmap --stats=10s --stats-one-line -vP --transfers=1 --min-size 100M --log-file=/home/qbt/bt_upload.log
   RCLONE_EXIT_CODE=$?
   if [ ${RCLONE_EXIT_CODE} -eq 0 ]; then
     cat >> /home/qbt/qb.log << EOF
