ffac1d7ddbd13a8d8efc9df7f25facbe2a91232b
cgking <58586625+cgkings@users.noreply.github.com>
2021-08-26 11:53:19 +0800

    Update cg_gdbak.sh

--- script/cg_gdbak.sh
+++ script/cg_gdbak.sh
@@ -18,6 +18,7 @@
 source <(curl -sL git.io/cg_script_option)
 setcolor
 curr_date=$(date "+%Y-%m-%d %H:%M:%S")
+bak_gd_remote="gd-"$bak_gd_remote""
 check_sys
 check_rclone
 : > /root/gdbak_log.log
@@ -33,28 +34,28 @@ check_run(){
 
 #gd间备份personal to personal 1
 echo -e "$curr_date [gd to gd]:personal to personal 1" | tee -a /root/gdbak_log.log
-fclone sync frreq:{0AOoUhVD6ULIfUk9PVA} frreq:{0AM2AXmxuonynUk9PVA} --drive-server-side-across-configs --stats=1s --stats-one-line -P --checkers=256 --transfers=320 --drive-pacer-min-sleep=1ms --drive-pacer-burst=1000 --drive-use-trash=false --check-first
+fclone sync "$bak_gd_remote":{0AOoUhVD6ULIfUk9PVA} "$bak_gd_remote":{0AM2AXmxuonynUk9PVA} --drive-server-side-across-configs --stats=1s --stats-one-line -P --checkers=256 --transfers=320 --drive-pacer-min-sleep=1ms --drive-pacer-burst=1000 --drive-use-trash=false --check-first
 check_run
 
 #gd间备份personal 1 to personal bak
 echo -e "$curr_date [gd to gd]:personal 1 to personal bak" | tee -a /root/gdbak_log.log
-fclone sync frreq:{0AM2AXmxuonynUk9PVA} frreq:{0AL1vRw5scrxmUk9PVA} --drive-server-side-across-configs --stats=1s --stats-one-line -P --checkers=256 --transfers=320 --drive-pacer-min-sleep=1ms --drive-pacer-burst=1000 --drive-use-trash=false --check-first
+fclone sync "$bak_gd_remote":{0AM2AXmxuonynUk9PVA} "$bak_gd_remote":{0AL1vRw5scrxmUk9PVA} --drive-server-side-across-configs --stats=1s --stats-one-line -P --checkers=256 --transfers=320 --drive-pacer-min-sleep=1ms --drive-pacer-burst=1000 --drive-use-trash=false --check-first
 check_run
 
 #gd to od
 echo -e "$curr_date [gd to od]:personal to odjav bak" | tee -a /root/gdbak_log.log
-fclone sync frreq:{1qitlvImjpef9NMQ1vV8Z2Qf0vLQc-3jL} odjav: --fast-list --drive-use-trash=false --stats=5s --stats-one-line -P --cache-chunk-size 128M --bwlimit 120M --max-size 100G --ignore-existing --ignore-checksum --ignore-size --ignore-errors --check-first
+fclone sync "$bak_gd_remote":{1qitlvImjpef9NMQ1vV8Z2Qf0vLQc-3jL} ode5-jav: --fast-list --drive-use-trash=false --stats=5s --stats-one-line -P --cache-chunk-size 128M --bwlimit 120M --max-size 100G --ignore-existing --ignore-checksum --ignore-size --ignore-errors --check-first
 check_run
 
 #gd to china od
 echo -e "$curr_date [gd to china od]:personal to wod 影视" | tee -a /root/gdbak_log.log
-rclone sync frreq:"/1 影视" wod:/影视 --drive-root-folder-id "0AOoUhVD6ULIfUk9PVA" --fast-list --drive-use-trash=false --stats=5s --stats-one-line --transfers=4 -P --cache-chunk-size 128M --bwlimit 120M --max-size 100G --ignore-existing --ignore-checksum --ignore-size --ignore-errors --check-first
+rclone sync "$bak_gd_remote":"/1 影视" wod:/影视 --drive-root-folder-id "0AOoUhVD6ULIfUk9PVA" --fast-list --drive-use-trash=false --stats=5s --stats-one-line --transfers=4 -P --cache-chunk-size 128M --bwlimit 120M --max-size 100G --ignore-existing --ignore-checksum --ignore-size --ignore-errors --check-first
 check_run
 
 echo -e "$curr_date [gd to china od]:personal to wod 剧集" | tee -a /root/gdbak_log.log
-rclone sync frreq:"/2 剧集" wod:/剧集 --drive-root-folder-id "0AOoUhVD6ULIfUk9PVA" --fast-list --drive-use-trash=false --stats=5s --stats-one-line --transfers=4 -P --cache-chunk-size 128M --bwlimit 120M --max-size 100G --ignore-existing --ignore-checksum --ignore-size --ignore-errors --check-first
+rclone sync "$bak_gd_remote":"/2 剧集" wod:/剧集 --drive-root-folder-id "0AOoUhVD6ULIfUk9PVA" --fast-list --drive-use-trash=false --stats=5s --stats-one-line --transfers=4 -P --cache-chunk-size 128M --bwlimit 120M --max-size 100G --ignore-existing --ignore-checksum --ignore-size --ignore-errors --check-first
 check_run
 
 echo -e "$curr_date [gd to china od]:personal to wod 动漫" | tee -a /root/gdbak_log.log
-rclone sync frreq:"/3 动漫" wod:/动漫 --drive-root-folder-id "0AOoUhVD6ULIfUk9PVA" --fast-list --drive-use-trash=false --stats=5s --stats-one-line --transfers=4 -P --cache-chunk-size 128M --bwlimit 120M --max-size 100G --ignore-existing --ignore-checksum --ignore-size --ignore-errors --check-first
+rclone sync "$bak_gd_remote":"/3 动漫" wod:/动漫 --drive-root-folder-id "0AOoUhVD6ULIfUk9PVA" --fast-list --drive-use-trash=false --stats=5s --stats-one-line --transfers=4 -P --cache-chunk-size 128M --bwlimit 120M --max-size 100G --ignore-existing --ignore-checksum --ignore-size --ignore-errors --check-first
 check_run
