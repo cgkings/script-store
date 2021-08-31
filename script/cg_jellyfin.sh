4f38abce88323d677d6ab34b97bf0fbc36a668fe
cgking <58586625+cgkings@users.noreply.github.com>
2021-08-31 09:05:05 +0800

    Update cg_jellyfin.sh

--- script/cg_jellyfin.sh
+++ script/cg_jellyfin.sh
@@ -58,7 +58,7 @@ initialization() {
     wget -O - https://repo.jellyfin.org/jellyfin_team.gpg.key | sudo apt-key add -
     echo "deb [arch=$( dpkg --print-architecture)] https://repo.jellyfin.org/$(  awk -F'=' '/^ID=/{ print $NF }' /etc/os-release) $(  awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release) main"  | sudo tee /etc/apt/sources.list.d/jellyfin.list
     sudo apt update
-    sudo apt install -y jellyfin
+    sudo apt install -y jellyfin > /dev/null
     sleep 1s
     echo -e "${curr_date} [INFO] jellyfin 安装成功，请访问：http://${ip_addr}:8096 进一步配置" | tee -a /root/install_log.txt
   fi
