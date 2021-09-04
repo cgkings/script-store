5d784f833fee0e3befb9449955077b0d7282a018
cgking <cgkings@gmail.com>
2021-09-04 11:26:38 +0800

    update

--- script/cg_fq.sh
+++ script/cg_fq.sh
@@ -7,10 +7,49 @@ v2ray_install() {
   #Created symlink /etc/systemd/system/multi-user.target.wants/v2ray.service → /etc/systemd/system/v2ray.service
   #/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
   rm -rf /usr/local/etc/v2ray/config.json
-  wget -qN https://golang.org/dl/go1.15.6.linux-amd64.tar.gz -O /usr/local/etc/v2ray/config.json
+  wget -qN https://raw.githubusercontent.com/cgkings/script-store/master/config/v2ray/config.json -O /usr/local/etc/v2ray/config.json
   systemctl enable v2ray && systemctl start v2ray
 }
 
 v2ray_uninstall() {
   bash <(curl -sL https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh) --remove
 }
+
+################## 脚本参数帮助 ##################
+v2ray_help() {
+  cat << EOF
+用法(Usage):
+  bash <(curl -sL git.io/cg_fq) [flags]
+
+可用参数(Available flags)：
+  bash <(curl -sL git.io/cg_fq) -i  安装v2ray
+  bash <(curl -sL git.io/cg_fq) -u  卸载v2ray
+  bash <(curl -sL git.io/cg_fq) -h  命令帮助
+注：无参数则默认为安装v2ray
+EOF
+}
+
+################## 执  行  命  令 ##################
+check_sys
+if [ -z "$1" ]; then
+  v2ray_install
+else
+  case "$1" in
+    -i | -I)
+      echo
+      v2ray_install
+      ;;
+    -u | -U)
+      echo
+      v2ray_uninstall
+      ;;
+    -h | -H)
+      echo
+      v2ray_help
+      ;;
+    *)
+      echo
+      v2ray_help
+      ;;
+  esac
+fi
