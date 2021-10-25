a23a38a13252b47fe9b9a7b442471a9737bb7b33
cgking <58586625+cgkings@users.noreply.github.com>
2021-10-25 17:22:39 +0800

    Update cg_speedtest.sh

--- script/cg_speedtest.sh
+++ script/cg_speedtest.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+#=============================================================
+# https://github.com/cgkings/script-store
+# bash <(curl -sL git.io/cg_speedtest)
+# File Name: cg_qbt.sh
+# Author: cgking
+# Created Time : 2021.6.21
+# Description:qbittonrrent脚本
+# System Required: Debian/Ubuntu
+# Version: final
+#=============================================================
 
 # Colors
 RED='\033[0;31m'
@@ -114,7 +124,7 @@ speed_test(){
 
 preinfo() {
 	echo "———————————————————SuperSpeed 全面测速版——————————————————"
-	echo "       bash <(curl -Lso- https://git.io/Jlkmw)"
+	echo "       bash <(curl -sL git.io/cg_speedtest)"
 	echo "       全部节点列表:  https://git.io/superspeedList"
 	echo "       节点更新: 2020/04/09  | 脚本更新: 2021/07/22"
 	echo "——————————————————————————————————————————————————————————"
@@ -338,54 +348,54 @@ runtest() {
 		echo "ID    测速服务器信息       上传/Mbps   下载/Mbps   延迟/ms"
 		start=$(date +%s) 
 
-		 speed_test '30154' '上海' '移动'
-		 speed_test '25637' '上海５Ｇ' '移动'
-		 speed_test '26728' '云南昆明' '移动'
-		 speed_test '27019' '内蒙古呼和浩特' '移动'
-		 speed_test '30232' '内蒙呼和浩特５Ｇ' '移动'
-		 speed_test '30293' '内蒙古通辽５Ｇ' '移动'
-		 speed_test '25858' '北京' '移动'
-		 speed_test '16375' '吉林长春' '移动'
-		 speed_test '24337' '四川成都' '移动'
-		 speed_test '17184' '天津５Ｇ' '移动'
-		 speed_test '26940' '宁夏银川' '移动'
-		 speed_test '31815' '宁夏银川' '移动'
+		#  speed_test '30154' '上海' '移动'
+		#  speed_test '25637' '上海５Ｇ' '移动'
+		#  speed_test '26728' '云南昆明' '移动'
+		#  speed_test '27019' '内蒙古呼和浩特' '移动'
+		#  speed_test '30232' '内蒙呼和浩特５Ｇ' '移动'
+		#  speed_test '30293' '内蒙古通辽５Ｇ' '移动'
+		#  speed_test '25858' '北京' '移动'
+		#  speed_test '16375' '吉林长春' '移动'
+		#  speed_test '24337' '四川成都' '移动'
+		#  speed_test '17184' '天津５Ｇ' '移动'
+		#  speed_test '26940' '宁夏银川' '移动'
+		#  speed_test '31815' '宁夏银川' '移动'
 		 speed_test '26404' '安徽合肥５Ｇ' '移动'
-		 speed_test '27151' '山东临沂５Ｇ' '移动'
-		 speed_test '25881' '山东济南５Ｇ' '移动'
-		 speed_test '27100' '山东青岛５Ｇ' '移动'
-		 speed_test '26501' '山西太原５Ｇ' '移动'
+		#  speed_test '27151' '山东临沂５Ｇ' '移动'
+		#  speed_test '25881' '山东济南５Ｇ' '移动'
+		#  speed_test '27100' '山东青岛５Ｇ' '移动'
+		#  speed_test '26501' '山西太原５Ｇ' '移动'
 		 speed_test '31520' '广东中山' '移动'
 		 speed_test '6611' '广东广州' '移动'
 		 speed_test '4515' '广东深圳' '移动'
-		 speed_test '15863' '广西南宁' '移动'
-		 speed_test '16858' '新疆乌鲁木齐' '移动'
-		 speed_test '26938' '新疆乌鲁木齐５Ｇ' '移动'
-		 speed_test '17227' '新疆和田' '移动'
-		 speed_test '17245' '新疆喀什' '移动'
-		 speed_test '17222' '新疆阿勒泰' '移动'
-		 speed_test '27249' '江苏南京５Ｇ' '移动'
-		 speed_test '21845' '江苏常州５Ｇ' '移动'
-		 speed_test '26850' '江苏无锡５Ｇ' '移动'
-		 speed_test '17320' '江苏镇江５Ｇ' '移动'
-		 speed_test '25883' '江西南昌５Ｇ' '移动'
-		 speed_test '17223' '河北石家庄' '移动'
-		 speed_test '26331' '河南郑州５Ｇ' '移动'
-		 speed_test '6715' '浙江宁波５Ｇ' '移动'
-		 speed_test '4647' '浙江杭州' '移动'
-		 speed_test '16503' '海南海口' '移动'
-		 speed_test '28491' '湖南长沙５Ｇ' '移动'
-		 speed_test '16145' '甘肃兰州' '移动'
-		 speed_test '16171' '福建福州' '移动'
-		 speed_test '18444' '西藏拉萨' '移动'
-		 speed_test '16398' '贵州贵阳' '移动'
-		 speed_test '25728' '辽宁大连' '移动'
-		 speed_test '16167' '辽宁沈阳' '移动'
-		 speed_test '17584' '重庆' '移动'
-		 speed_test '26380' '陕西西安' '移动'
-		 speed_test '29105' '陕西西安５Ｇ' '移动'
-		 speed_test '29083' '青海西宁５Ｇ' '移动'
-		 speed_test '26656' '黑龙江哈尔滨' '移动'
+		#  speed_test '15863' '广西南宁' '移动'
+		#  speed_test '16858' '新疆乌鲁木齐' '移动'
+		#  speed_test '26938' '新疆乌鲁木齐５Ｇ' '移动'
+		#  speed_test '17227' '新疆和田' '移动'
+		#  speed_test '17245' '新疆喀什' '移动'
+		#  speed_test '17222' '新疆阿勒泰' '移动'
+		#  speed_test '27249' '江苏南京５Ｇ' '移动'
+		#  speed_test '21845' '江苏常州５Ｇ' '移动'
+		#  speed_test '26850' '江苏无锡５Ｇ' '移动'
+		#  speed_test '17320' '江苏镇江５Ｇ' '移动'
+		#  speed_test '25883' '江西南昌５Ｇ' '移动'
+		#  speed_test '17223' '河北石家庄' '移动'
+		#  speed_test '26331' '河南郑州５Ｇ' '移动'
+		#  speed_test '6715' '浙江宁波５Ｇ' '移动'
+		#  speed_test '4647' '浙江杭州' '移动'
+		#  speed_test '16503' '海南海口' '移动'
+		#  speed_test '28491' '湖南长沙５Ｇ' '移动'
+		#  speed_test '16145' '甘肃兰州' '移动'
+		#  speed_test '16171' '福建福州' '移动'
+		#  speed_test '18444' '西藏拉萨' '移动'
+		#  speed_test '16398' '贵州贵阳' '移动'
+		#  speed_test '25728' '辽宁大连' '移动'
+		#  speed_test '16167' '辽宁沈阳' '移动'
+		#  speed_test '17584' '重庆' '移动'
+		#  speed_test '26380' '陕西西安' '移动'
+		#  speed_test '29105' '陕西西安５Ｇ' '移动'
+		#  speed_test '29083' '青海西宁５Ｇ' '移动'
+		#  speed_test '26656' '黑龙江哈尔滨' '移动'
 
 		end=$(date +%s)  
 		####rm -rf speedtest*
