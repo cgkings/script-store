864a74b34166e465bd1ea1f224b6f6c0b58095d5
cgking <cgkings@gmail.com>
2021-12-03 15:24:54 +0800

    Update cg_sub.sh

--- script/cg_sub.sh
+++ script/cg_sub.sh
@@ -4,4 +4,6 @@ apt-get install -y nodejs npm
 npm install -g yarn
 cd /root && git clone https://github.com/CareyWang/sub-web.git && cd sub-web && yarn install
 yarn config set ignore-engines true
-sed -i 's/http\:\/\/127.0.0.1:25500/https\:\/\/x-clash.gq/g' /root/sub-web/src/views/Subconverter.vue
+yarn build
+cd /root && wget https://github.com/tindy2013/subconverter/releases/download/v0.6.3/subconverter_linux64.tar.gz && tar -zxvf subconverter_linux64.tar.gz && rm -f subconverter_linux64.tar.gz
+systemctl daemon-reload && systemctl start sub && systemctl enable sub && systemctl status sub
