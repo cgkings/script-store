a7b6e921a983b35ae1104dfc681cfe15fad66baf
cgking <cgkings@gmail.com>
2021-12-05 16:10:59 +0800

    Delete cg_sub.sh

--- script/cg_sub.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-apt update && apt-get upgrade
-
-cd /root && git clone https://github.com/CareyWang/sub-web.git && cd sub-web && yarn install
-yarn config set ignore-engines true
-yarn build
-cd /root && wget https://github.com/tindy2013/subconverter/releases/download/v0.6.3/subconverter_linux64.tar.gz && tar -zxvf subconverter_linux64.tar.gz && rm -f subconverter_linux64.tar.gz
-systemctl daemon-reload && systemctl start sub && systemctl enable sub && systemctl status sub
