12c7b16de2674a44bcbb8840f25f7f436c7416bf
cgking <58586625+cgkings@users.noreply.github.com>
2022-07-18 16:28:13 +0800

    Update cg_qbt.sh

--- script/cg_qbt.sh
+++ script/cg_qbt.sh
@@ -25,7 +25,7 @@ file_hash=$5 #%I
 file_category=$6 #%L: 分类
 qpt_username="admin"
 qpt_password="adminadmin"
-qb_web_url="http://$(curl -sL ifconfig.me):8070"
+qb_web_url="http://$(curl -sL ifconfig.me):18080"
 rclone_remote="upsa"
 
 ################## rclone上传模块 ##################
