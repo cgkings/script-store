a29bc8ffe204dc5a944ce8068e5fcc257027da86
cgking <58586625+cgkings@users.noreply.github.com>
2022-11-03 13:56:03 +0800

    Update cg_pve.sh

--- script/cg_pve.sh
+++ script/cg_pve.sh
@@ -4,8 +4,8 @@
 # bash <(curl -sL git.io/cg_pve)
 # File Name: automount
 # Author: cgkings
-# Created Time : 2020.12.25
-# Description:挂载一键脚本
+# Created Time : 2022.11.3
+# Description:PVE一键脚本
 # System Required: Debian/Ubuntu
 # Version: 1.0
 #=============================================================
@@ -20,20 +20,23 @@ setcolor
 
 ################## 前置变量设置 ##################
 install_pve() {
-cat > /etc/hosts << EOF
+  apt update --fix-missing 2> /dev/null | grep packages | cut -d '.' -f 1
+  apt install -y curl sudo git make wget tree vim nano tmux htop net-tools parted nethogs screen ntpdate manpages-zh screenfetch file virt-what iperf3 jq expect 2> /dev/null
+  apt install -y ca-certificates dmidecode findutils dpkg tar zip unzip gzip bzip2 unar p7zip-full pv ffmpeg build-essential ncdu zsh fonts-powerline fuse 2> /dev/null
+  cat > /etc/hosts << EOF
 127.0.0.1       localhost localhost.localdomain
-$(curl -sL ifconfig.me)       $(hostnamectl|grep hostname|awk '{print $3}') $(hostnamectl|grep hostname|awk '{print $3}').proxmox.com
+$(curl -sL ifconfig.me)       $(hostnamectl | grep hostname | awk '{print $3}') $(hostnamectl | grep hostname | awk '{print $3}').proxmox.com
 
 # The following lines are desirable for IPv6 capable hosts
 ::1     localhost localhost.localdomain
 ff02::1 ip6-allnodes
 ff02::2 ip6-allrouters
 EOF
-echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bullseye pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
-wget -q https://enterprise.proxmox.com/debian/proxmox-release-bullseye.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg
-apt update 2>/dev/null | grep packages | cut -d '.' -f 1
-apt full-upgrade -y 2> /dev/null | grep upgraded
-apt install -y proxmox-ve postfix open-iscsi 2>/dev/null
-
-
+  echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bullseye pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
+  wget -q https://enterprise.proxmox.com/debian/proxmox-release-bullseye.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg
+  apt update 2> /dev/null | grep packages | cut -d '.' -f 1 && apt full-upgrade -y 2> /dev/null | grep upgraded
+  apt install -y proxmox-ve postfix open-iscsi 2> /dev/null
+  systemctl reboot
 }
+
+install_pve
