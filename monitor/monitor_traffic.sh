8e87f1e093dc28b23f25091ea8cffaa4d95d054e
cgking <cgkings@gmail.com>
2024-08-14 16:20:24 +0800

    更新 monitor_traffic.sh

--- monitor/monitor_traffic.sh
+++ monitor/monitor_traffic.sh
@@ -14,7 +14,7 @@ SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SO
 INTERFACE=$(ip route | grep default | awk '{print $5}')
 
 # 确保日志目录存在
-mkdir -p "$(dirname "$LOG_FILE")"
+mkdir -p $(dirname "$LOG_FILE")
 # =============================================================================
 
 # ============================ 检查并设置计划任务 ============================
@@ -30,26 +30,31 @@ fi
 # ============================ 检查并重置流量统计 =============================
 if [ ! -f "$MONTHLY_LOG" ] || [ "$(cat $MONTHLY_LOG)" != "$CURRENT_MONTH" ]; then
     echo "$CURRENT_MONTH" > "$MONTHLY_LOG"
-    vnstat --disable -i "$INTERFACE"
-    vnstat --enable -i "$INTERFACE"  # 重置vnStat的流量统计
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - Reset monthly traffic statistics" >> $LOG_FILE
+    
+    # 重置vnstat流量统计
+    sudo systemctl stop vnstat
+    sudo rm /var/lib/vnstat/${INTERFACE}
+    sudo systemctl start vnstat
+
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Reset monthly traffic statistics by deleting vnstat database" >> $LOG_FILE
 fi
 # =============================================================================
 
 # ============================ 流量监控和操作 ================================
 VNSTAT_JSON=$(vnstat -i "$INTERFACE" --json)
-RX=$(echo "$VNSTAT_JSON" | jq -r '.interfaces[0].traffic.total.rx')
-TX=$(echo "$VNSTAT_JSON" | jq -r '.interfaces[0].traffic.total.tx')
+RX=$(echo "$VNSTAT_JSON" | jq -r '.interfaces[0].traffic.total.rx')  # 获取接收流量，单位为MiB
+TX=$(echo "$VNSTAT_JSON" | jq -r '.interfaces[0].traffic.total.tx')  # 获取发送流量，单位为MiB
 
 # 确保RX和TX是有效数字
-if ! [[ $RX =~ ^[0-9]+$ ]] || ! [[ $TX =~ ^[0-9]+$ ]]; then
+if ! [[ $RX =~ ^[0-9]+(\.[0-9]+)?$ ]] || ! [[ $TX =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: RX or TX is not a valid number" >> $LOG_FILE
     exit 1
 fi
 
-RX_GB=$(echo "scale=2; $RX / 1024 / 1024" | bc)
-TX_GB=$(echo "scale=2; $TX / 1024 / 1024" | bc)
-TOTAL=$(echo "scale=2; ($RX + $TX) / 1024 / 1024" | bc)
+# 直接使用MiB单位
+RX_GB=$(echo "scale=2; $RX / 1024" | bc)  # 将MiB转换为GB
+TX_GB=$(echo "scale=2; $TX / 1024" | bc)  # 将MiB转换为GB
+TOTAL=$(echo "scale=2; $RX_GB + $TX_GB" | bc)  # 总流量GB
 
 # 定义流量信息输出函数，带有一个参数作为附加信息
 log_traffic_info() {
@@ -66,6 +71,7 @@ EOF
 # 判断是否超过流量限制并执行相应操作
 if (( $(echo "$TOTAL >= $LIMIT2" | bc -l) )); then
     log_traffic_info "已超过160GB，执行关机操作！！！"
+    sudo shutdown -h now
 elif (( $(echo "$TOTAL >= $LIMIT" | bc -l) )); then
     log_traffic_info "已超过150GB，超过160GB将执行关机操作！！！"
 else
